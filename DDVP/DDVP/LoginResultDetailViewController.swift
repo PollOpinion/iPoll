c6e23661a6494de2c2fb95722bd1279bc49e5e1b
PankajNeve <pankajneve@pankaj-mac-mini-tit4aa164.local>
2017-01-20 16:58:28 +0530

    Back button on Profile screen is made hidden as required.

--- DDVP/DDVP/LoginResultDetailViewController.swift
+++ DDVP/DDVP/LoginResultDetailViewController.swift
@@ -30,6 +30,7 @@ class LoginResultDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationItem.hidesBackButton = true
         
         if pollUser?.UserProvider == UserProvider.facebook {
         
