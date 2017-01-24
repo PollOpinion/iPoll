c4fe9c345e1442ffde0dcdbc2ee84c3dbd735490
PankajNeve <pankajneve@pankaj-mac-mini-tit4aa164.local>
2017-01-24 18:55:00 +0530

    Added Question deletion to EventDetailVC

--- DDVP/DDVP/PresenterEventDetailVC.swift
+++ DDVP/DDVP/PresenterEventDetailVC.swift
@@ -162,27 +162,25 @@ class PresenterEventDetailVC: UITableViewController {
         
     }
     
-    
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            eventsArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            self.deleteQuestionFromFirebase(atIndex: indexPath.row)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
@@ -219,5 +217,10 @@ class PresenterEventDetailVC: UITableViewController {
         let uploadData : [String : Any] = question.toAnyObject() as! [String : Any];
         FirebaseManager.sharedInstance.uploadQuestionAtChannel(eventName: self.eventName, withData: uploadData)
         //self.tableView.reloadData()
+    }
+    
+    func deleteQuestionFromFirebase(atIndex:Int) {
+        // TODO : delete Question from  Firebase
+        print("TODO : delete Question from  Firebase")
     }
 }
